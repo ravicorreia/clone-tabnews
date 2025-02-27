83e3f075966dd732d29427f51ae32473b274fcea
ravicorreia <ravicorreia@proton.me>
2025-02-27 15:51:00 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1> Olá Pipoquinha, se você me ama, dê uma risadinha! </h1>;
+  return <h1> Olá Pipoquinha, se você me ama, dê uma risadinha! =) </h1>;
 }
 
 export default Home;
