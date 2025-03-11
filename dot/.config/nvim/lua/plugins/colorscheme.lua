69c3440a6d3147baed7d702e68e8d4ba4e60e977
ravicorreia <ravicorreia@proton.me>
2025-03-11 00:12:38 -0300

    update

--- dot/.config/nvim/lua/plugins/colorscheme.lua
+++ dot/.config/nvim/lua/plugins/colorscheme.lua
@@ -9,7 +9,7 @@ return {
       local rose_pine = require("rose-pine")
       rose_pine.setup({
         opt = true,
-        variant = "auto", -- auto, main, moon, or dawn
+        variant = "auto",      -- auto, main, moon, or dawn
         dark_variant = "main", -- main, moon, or dawn
         dim_inactive_windows = false,
         extend_background_behind_borders = true,
@@ -17,13 +17,13 @@ return {
         enable = {
           terminal = true,
           legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-          migrations = true, -- Handle deprecated options automatically
+          migrations = true,        -- Handle deprecated options automatically
         },
 
         styles = {
           bold = true,
           italic = true,
-          transparency = false,
+          transparency = true,
         },
 
         groups = {
@@ -102,7 +102,7 @@ return {
         opt = true,
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        transparent = true, -- Enable this to disable setting the background color
+        transparent = true,     -- Enable this to disable setting the background color
         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
         styles = {
           -- Style to be applied to different syntax groups
@@ -112,14 +112,14 @@ return {
           functions = {},
           variables = {},
           -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "dark", -- style for sidebars, see below
-          floats = "transparent", -- style for floating windows
+          sidebars = "dark",              -- style for sidebars, see below
+          floats = "transparent",         -- style for floating windows
         },
-        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
         hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = false, -- dims inactive windows
-        lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+        dim_inactive = false,             -- dims inactive windows
+        lualine_bold = true,              -- When `true`, section headers in the lualine theme will be bold
 
         --- You can override specific color groups to use other groups or a hex color
         --- function will be called with a ColorScheme table
@@ -155,17 +155,17 @@ return {
     config = function()
       local kanagawa = require("kanagawa")
       kanagawa.setup({
-        compile = false, -- enable compiling the colorscheme
+        compile = false,  -- enable compiling the colorscheme
         undercurl = true, -- enable undercurls
         commentStyle = { italic = true },
         functionStyle = {},
         keywordStyle = { italic = true },
         statementStyle = { bold = true },
         typeStyle = {},
-        transparent = true, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        transparent = true,    -- do not set background color
+        dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
         terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = { -- add/modify theme and palette colors
+        colors = {             -- add/modify theme and palette colors
           palette = {},
           theme = { wave = {}, lotus = {}, dragon = {}, all = { bg_gutter = "none" } },
         },
@@ -202,8 +202,8 @@ return {
             TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
           }
         end,
-        theme = "dragon", -- Load "wave" theme when 'background' option is not set
-        background = { -- map the value of 'background' option to a theme
+        theme = "dragon",  -- Load "wave" theme when 'background' option is not set
+        background = {     -- map the value of 'background' option to a theme
           dark = "dragon", -- try "dragon" !
           light = "lotus",
         },
